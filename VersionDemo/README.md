9a90202c712890682abeaf58646c097991868362
Isabel <isabel@hermit.no>
2022-03-16 18:30:51 +0100

    start

---
+++ VersionDemo/README.md
@@ -0,0 +1,3 @@
+# VersionDemo
+
+![](https://img.shields.io/endpoint?url=https://versiondemoisabel.azurewebsites.net/api/info)
---
+++ VersionDemo/VersionDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace VersionDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public HtmlString Version { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Version = new HtmlString(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            
+        }
+
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
